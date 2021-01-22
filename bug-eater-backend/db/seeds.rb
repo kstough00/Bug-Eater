0ced48b2bc0092417acd238f29f88137175a4334
Your Username <you@example.com>
2021-01-22 14:27:58 -0700

    db seeded

--- bug-eater-backend/db/seeds.rb
+++ bug-eater-backend/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Faker::Quote.famous_last_words #=> "My vocabulary did this to me. Your love will let you go on…"
+
+# Faker::Quote.jack_handey #=> "I hope life isn't a big joke, because I don't get it."
+
+# Faker::Quote.matz #=> "You want to enjoy life, don't you? If you get your job done quickly and your job is fun, that's good isn't it? That's the purpose of life, partly. Your life is better."
+
+# Faker::Quote.most_interesting_man_in_the_world #=> "He can speak Russian… in French"
+
+# Faker::Quote.robin #=> "Holy Razors Edge"
+
+# Faker::Quote.singular_siegler #=> "Texas!"
+
+# Faker::Quote.yoda #=> "Use your feelings, Obi-Wan, and find him you will."
+
+10.times do
+    Bug.create(title: Faker::Movie.title, 
+        description:Faker::Movie.quote, 
+        severity:Faker::Number.number(digits: 5), 
+        resolved:false)
+end
