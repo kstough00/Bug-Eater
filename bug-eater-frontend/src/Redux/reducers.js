da7c709de541286d7b6067bf324d7a9b70735733
Your Username <you@example.com>
2021-02-04 16:42:34 -0700

    bugreducer first action case added

--- bug-eater-frontend/src/Redux/reducers.js
+++ bug-eater-frontend/src/Redux/reducers.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function positionReducer() {
-    return (
-        <div>
-            
-        </div>
-    )
+function bugReducer(state = { bugs: [] }, action) {
+    switch (action.type) {
+        case "FETCH_BUGS_SUCCESS":
+            return { ...state, bugs: action.payload };
+        case ""
+        default:
+        return state;
+    }
 }
+
+export default bugReducer;
