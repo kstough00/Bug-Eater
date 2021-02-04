8dafcc4b18f1b90b43a03f284a304f36a3b22348
Your Username <you@example.com>
2021-02-04 16:23:59 -0700

    app.js and routes created

--- bug-eater-frontend/src/App.js
+++ bug-eater-frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home'
+import BugsIndex from './components/BugsIndex'
+import BugsForm from './components/BugsForm'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/bugs" component={BugsIndex}/>
+          <Route path="/bugs/new" component={BugsForm}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
